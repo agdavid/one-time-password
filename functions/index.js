d343400e0ae73c7dace99dad2bfa1de968128f7a
Ajay David <antonio.g.david@gmail.com>
2017-07-05 15:42:41 -0400

    import create_user to index

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const createUser = require('./create_user');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,6 +8,5 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
-exports.goodBye = functions.https.onRequest((request, response) => {
-  response.send("Goodbye!");
-});
+// on http request, run createUser
+exports.createUser = functions.https.onRequest(createUser);
